c3a0aacd2c0b21d376c916c01e3c54aea73d703f
pro_c <hieutg244@gmail.com>
2025-03-09 16:11:25 +0700

    remove iap

--- Assets/_App/Scripts/CoinManager/PurchasingManager.cs
+++ Assets/_App/Scripts/CoinManager/PurchasingManager.cs
@@ -8,22 +8,7 @@ public class PurchasingManager : MonoBehaviour
    {
       switch (i)
       {
-         case 1:
-            GameDataManager.Instance.playerData.AddDiamond(1);
-             IAPManager.Instance.BuyProductID(IAPKey.PACK1);
-            break;
-         case 2:
-            GameDataManager.Instance.playerData.AddDiamond(3);
-            IAPManager.Instance.BuyProductID(IAPKey.PACK2);
-            break;
-         case 3:
-            GameDataManager.Instance.playerData.AddDiamond(5);
-            IAPManager.Instance.BuyProductID(IAPKey.PACK3);
-            break;
-         case 4:
-            GameDataManager.Instance.playerData.AddDiamond(10);
-            IAPManager.Instance.BuyProductID(IAPKey.PACK4);
-            break;
+      
       }
    }
 
