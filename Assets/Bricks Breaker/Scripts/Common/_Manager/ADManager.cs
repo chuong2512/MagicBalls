6d376f30766fae0fd2c16ea7b4daa4c5736742c1
pro_c <hieutg244@gmail.com>
2025-03-09 12:58:14 +0700

    init prj

---
+++ Assets/Bricks Breaker/Scripts/Common/_Manager/ADManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class ADManager : Singleton<ADManager>
+{
+    string gameId = "1234567";
+    string myPlacementId = "rewardedVideo";
+    bool testMode = true;
+
+    /// <summary>
+    /// Initialize the Ads listener and service
+    /// </summary>
+    void Start()
+    {
+     
+    }
+    
+    
+    /*/// <summary>
+    /// Rewarded video ads
+    /// </summary>
+    public void ShowRewardedVideo(Action<ShowResult> action)
+    {
+        this.action = action;
+
+        // Check if UnityAds ready before calling Show method:
+        if (Advertisement.IsReady(myPlacementId))
+        {
+            Advertisement.Show(myPlacementId);
+        }
+        else
+        {
+            Debug.Log("Rewarded video is not ready at the moment! Please try again later!");
+        }
+    }*/
+
+    /// <summary>
+    /// Implement IUnityAdsListener interface methods
+    /// </summary>
+   
+}
+
+
